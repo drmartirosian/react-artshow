bc520bfa93c1843571263101f2462fce808996c1
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2020-11-06 13:32:01 -0600

    1st

---
+++ src/components/ImgGallery.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import Img1 from '../img/1.jpg';
+
+function NavBar() {
+
+  return (
+    <div>
+      <Carousel >
+        <Carousel.Item interval={1000}>
+          <img
+            className="d-block w-100"
+            style={{'height':"500px"}} 
+            src={Img1}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={1000}>
+          <img
+            className="d-block w-100"
+            style={{'height':"500px"}} 
+            src={Img1}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>  
+        <Carousel.Item interval={1000}>
+          <img
+            className="d-block w-100"
+            style={{'height':"500px"}} 
+            src={Img1}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+}
+
+export default NavBar;
