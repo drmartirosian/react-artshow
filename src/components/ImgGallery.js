ce270e4a0919259c14668bcbc3cd73747a4f6dea
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2021-01-18 01:20:02 -0600

    19 images in carousel

--- src/components/ImgGallery.js
+++ src/components/ImgGallery.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
 import Img1 from '../img/1.jpg';
+import Img2 from '../img/2.jpg';
+import Img3 from '../img/3.jpg';
+import Img4 from '../img/4.jpg';
+import Img5 from '../img/5.jpg';
+import Img6 from '../img/6.jpg';
+import Img7 from '../img/7.jpg';
+import Img8 from '../img/8.jpg';
+import Img9 from '../img/9.jpg';
+import Img10 from '../img/10.jpg';
+import Img11 from '../img/11.jpg';
+import Img12 from '../img/12.jpg';
+import Img13 from '../img/13.jpg';
+import Img14 from '../img/14.jpg';
+import Img15 from '../img/15.jpg';
+import Img16 from '../img/16.jpg';
+import Img17 from '../img/17.jpg';
+import Img18 from '../img/18.jpg';
+import Img19 from '../img/19.jpg';
+
 
 function ImgGallery() {
 
@@ -23,7 +42,7 @@ function ImgGallery() {
           <img
             className="d-block w-100"
             style={{'height':"500px"}} 
-            src={Img1}
+            src={Img2}
             alt="First slide"
           />
           <Carousel.Caption>
@@ -35,7 +54,199 @@ function ImgGallery() {
           <img
             className="d-block w-100"
             style={{'height':"500px"}} 
-            src={Img1}
+            src={Img3}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>Three slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={1000}>
+          <img
+            className="d-block w-100"
+            style={{'height':"500px"}} 
+            src={Img4}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>Three slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={1000}>
+          <img
+            className="d-block w-100"
+            style={{'height':"500px"}} 
+            src={Img5}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>Three slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={1000}>
+          <img
+            className="d-block w-100"
+            style={{'height':"500px"}} 
+            src={Img6}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>Three slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={1000}>
+          <img
+            className="d-block w-100"
+            style={{'height':"500px"}} 
+            src={Img7}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>Three slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={1000}>
+          <img
+            className="d-block w-100"
+            style={{'height':"500px"}} 
+            src={Img8}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>Three slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={1000}>
+          <img
+            className="d-block w-100"
+            style={{'height':"500px"}} 
+            src={Img9}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>Three slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={1000}>
+          <img
+            className="d-block w-100"
+            style={{'height':"500px"}} 
+            src={Img10}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>Three slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={1000}>
+          <img
+            className="d-block w-100"
+            style={{'height':"500px"}} 
+            src={Img11}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>Three slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={1000}>
+          <img
+            className="d-block w-100"
+            style={{'height':"500px"}} 
+            src={Img12}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>Three slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={1000}>
+          <img
+            className="d-block w-100"
+            style={{'height':"500px"}} 
+            src={Img13}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>Three slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={1000}>
+          <img
+            className="d-block w-100"
+            style={{'height':"500px"}} 
+            src={Img14}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>Three slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={1000}>
+          <img
+            className="d-block w-100"
+            style={{'height':"500px"}} 
+            src={Img15}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>Three slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={1000}>
+          <img
+            className="d-block w-100"
+            style={{'height':"500px"}} 
+            src={Img16}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>Three slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={1000}>
+          <img
+            className="d-block w-100"
+            style={{'height':"500px"}} 
+            src={Img17}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>Three slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={1000}>
+          <img
+            className="d-block w-100"
+            style={{'height':"500px"}} 
+            src={Img18}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>Three slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={1000}>
+          <img
+            className="d-block w-100"
+            style={{'height':"500px"}} 
+            src={Img19}
             alt="First slide"
           />
           <Carousel.Caption>
