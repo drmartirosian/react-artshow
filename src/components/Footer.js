54a495e84a8891fbeb2d106eee86002db8a8b2ce
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2021-04-03 01:54:44 -0500

    info in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { Carousel } from 'react-bootstrap';
+import { Accordion, Card, Button } from 'react-bootstrap';
 
 
 function Footer() {
@@ -13,7 +13,23 @@ function Footer() {
       right:0,
       backgroundColor:'lightgray'
     }}>
-     <h1>Patty Martirosian, 2021™</h1>
+      <Accordion defaultActiveKey="0">
+
+  <Card>
+    <Card.Header>
+      <Accordion.Toggle as={Button} variant="link" eventKey="1">
+        CONTACT INFO
+      </Accordion.Toggle>
+    </Card.Header>
+    <Accordion.Collapse eventKey="1">
+      <Card.Body>
+        <p>EMAIL: the100acrewood@yahoo.com</p>
+        <p>PHONE: 619-908-0665</p>
+      </Card.Body>
+    </Accordion.Collapse>
+  </Card>
+</Accordion>
+     {/* <h1>Patty Martirosian, 2021™</h1> */}
     </div>
   );
 }
