fb2c01c3fb2fdb4438a4cef3b552faa5ee9a8598
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2021-04-03 02:09:22 -0500

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,14 +6,15 @@ function Banner() {
 
   return (
     <div style={{color:'white', backgroundColor:'#46676d', margin:'50px 50px 100px 50px', padding:'50px 50px 50px 50px'}}>
-      <h1>Art Gallery </h1> <p>Get your daily dose!</p>
+      <h1>Patty's Art Gallery </h1> 
+      <p>Get your daily dose!</p>
     <h3>
         Welcome! 
         <br/>
         There's much to see here. So, take your time, look around. 
         Then make sure to drop me a line.
         <br/>
-        I look forward to hearing from you.!
+        I look forward to hearing from you!
       </h3>
      
     </div>
